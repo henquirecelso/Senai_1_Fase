35668bdf89458196a995753393daab5ba9545edc
HENRIQUE CELSO DA SILVEIRA <henrique_silveira152@estudante.sesisenai.org.br>
2025-09-09 22:13:43 -0300

    Aula de javascript

---
+++ logica_js/variaveis.js
@@ -0,0 +1,13 @@
+// let nome = "joao";
+// nome = "henrique";
+
+// const idade = 62;
+// const nome = "henrique"
+// const masculino = true
+// const sobrenome = "da silveira"
+// console.log("meu nome é " + nome + " " + sobrenome)
+
+// console.log(`meu nome é ${nome} ${sobrenome} e tenho ${idade} anos`)
+
+
+const frutas = ["banana", "maçã", "laranja", "uva"];cd ..
