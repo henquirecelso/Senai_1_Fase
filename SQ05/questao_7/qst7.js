b7a1f41c1120c65eed6ec00c7aaa8f66efc34d46
HENRIQUE CELSO DA SILVEIRA <henrique_silveira152@estudante.sesisenai.org.br>
2025-09-22 22:15:47 -0300

    Aula Javascript

--- SQ05/questao_7/qst7.js
+++ SQ05/questao_7/qst7.js
@@ -1,9 +1,28 @@
 let diaria = Number(prompt("Digite o numero de quantas diarias vai ficar: "))
+let valorDiaria 
+let totalBruto
+let desconto, multa=150, totalLiquido
+desconto = totalBruto * 0.25
 
 if (diaria <= 5) {
-    alert("O preço total já com os descontos vai ser $" + (diaria * 100) * 0.25)
-} else if (diaria > 6 && diaria <= 10) {
-    alert("O preço total já com os descontos vai ser $" + (diaria * 90) * 0.25)
+    valorDiaria = 100
+    totalBruto = diaria * valorDiaria
+    desconto = totalBruto * 0.25
+    totalLiquido = totalBruto - desconto + multa
+    alert("O preço total já com os descontos e a multa vai ser $" + totalLiquido.toFixed(2).replace(".",","))
+} else if (diaria <= 10) {
+    valorDiaria = 90
+    totalBruto = diaria * valorDiaria
+    desconto = totalBruto * 0.25
+    totalLiquido = totalBruto - desconto + multa
+    alert("O preço total já com os descontos e a multa vai ser $" + totalLiquido.toFixed(2).replace(".",","))
 } else {
-    alert("O preço total já com os descontos vai ser $" + (diaria * 80) * 0.25)
-}
+    valorDiaria = 80
+    totalBruto = diaria * valorDiaria
+    desconto = totalBruto * 0.25
+    totalLiquido = totalBruto - desconto + multa
+    alert("O preço total já com os descontos e a multa vai ser $" + totalLiquido.toFixed(2).replace(".",","))
+
+    
+}   
+
