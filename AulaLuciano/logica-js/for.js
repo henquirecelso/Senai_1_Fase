0e931ed70f3650d6b129df50061a4f35fae0203d
HENRIQUE CELSO DA SILVEIRA <henrique_silveira152@estudante.sesisenai.org.br>
2025-09-30 22:12:40 -0300

    Aula Javascript

---
+++ AulaLuciano/logica-js/for.js
@@ -0,0 +1,35 @@
+// // // // for(let i = 0; i <= 10; i++){
+// // // //     console.log(i)
+// // // // }
+
+
+
+// // // for(let num = 10; num >= 0; num--){
+// // //     console.log(num)
+// // // }
+
+// // for(let num = 0; num <= 20; num +=2){
+// //     console.log(num)
+// // }
+
+
+// // let soma = 0 
+// // for(let i = 1; i<100; i++){
+// //     soma += i
+// // }
+// // console.log("soma: ", soma)
+
+
+
+// let alunos = ["João", "Maria", "Joana"]
+// alunos.push("Pedro")
+
+// for(let i = 0; i<alunos.length; i++){
+//     console.log("aluno: " + alunos[i])
+// }
+
+
+let frutas = ["Maça", "Banana", "Abacate", "tomate", "goiaba"]
+for(let i = 0; i<frutas.length; i++){
+    console.log("A fruta é: " + frutas[i])
+}
---
+++ AulaLuciano/logica-js/funcoes.js
@@ -0,0 +1,23 @@
+// // function saudacao(nome){
+// //     console.log(`Bom dia ${nome}`)
+// // } 
+// const saudacao = (nome)=>{
+//     console.log(`Bom dia ${nome}`)
+// }
+// saudacao("Policial")
+// saudacao("Oruam")
+
+const prompt = require("prompt-sync")()
+const soma = (x,y) => {
+  return x+y
+  console.log("teste")
+}
+let num1 = Number(prompt("Digite o primeiro numero: "))
+let num2 = Number(prompt("Digite o segundo numero: "))
+
+console.log(soma(num1,num2))
+
+
+
+
+
